bd70e7c025e5ea755b0d5998d4136980667c6c90
YuInseo <inseo0121@gmail.com>
2024-04-16 16:20:36 +0900

    feat: edit load page logic

--- app/lib/page/page.ts
+++ app/lib/page/page.ts
@@ -3,7 +3,6 @@ export async function DeletePage() {
 }
 
 export async function LoadPages(userUuid: string) {
-
     try {
         const res = await fetch(`http://localhost:8090/client/page/load/all/${userUuid}`, {
             method: 'POST'
@@ -11,55 +10,32 @@ export async function LoadPages(userUuid: string) {
 
         const value = await res.json();
 
-        const newPages: Page[] = value.map((element: any) => {
-            let page: Page;
-            let blocks: IBlock[] = [];
+        let pages: Page[] = [];
 
-            element.blocks.map((block: any) => {
-                let contents: IContent[] = [];
+        value.map((element: any) => {
+            let element_metadata = element.metadata;
+            let title = element_metadata.title;
+            let url = element._id.$oid;
 
-                block.content.map((content: any) => {
-                    const blockType = content.blockType;
-                    const styles = content.styles;
-                    const text = content.text;
+            let metadata: Metadata = {
+                url: url,
+                title: title
+            }
 
-                    let new_content: IContent = {
-                        blockType: blockType,
-                        text: text,
-                        styles: styles
-                    }
-
-                    contents.push(new_content);
-                });
-
-                let new_block: IBlock = {
-                    content: contents,
-                    value: "",
-                    type: "Text"
-                }
-
-                blocks.push(new_block)
-            });
-
-            const newMetadata: Metadata = {
-                url: element._id.$oid,
-                title: element.metadata.title
-            };
-
-            page = {
-                owner: element.owner,
-                metadata: newMetadata,
-                blocks: blocks,
-                guest: [],
+            let page: Page = {
+                owner: userUuid,
+                metadata: metadata,
+                blocks: [],
                 members: [],
+                guest: [],
                 isShared: false,
                 created_date: ""
-            };
+            }
 
-            return page;
+            pages.push(page);
+
         });
-
-        return newPages;
+        return pages;
     } catch (error) {
         throw error;
     }
