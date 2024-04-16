7e776c9c82a2436f68a93b410bcd1116f71c6026
YuInseo <inseo0121@gmail.com>
2024-04-16 16:23:11 +0900

    feat: refactoring client.tsx

--- app/components/props/client.tsx
+++ app/components/props/client.tsx
@@ -6,28 +6,25 @@ import {useEffect, useState} from "react";
 import {Navbar} from "@/app/components/ui/navbar";
 import {CommandMenu} from "@/app/components/props/command-demo";
 import {useAccount} from "@/app/hook/useAccount";
+import {cookies} from "next/headers";
 
 export const Client = ({uuid}: ClientProp) => {
-    let {data, isLoading} = useAccount();
 
-    let [test, setTest] = useState("false");
+    let {data} = useAccount();
+
     useEffect(() => {
-        if (!isLoading) {
-            console.log("data", data);
-        }
-
-        console.log(test as unknown as boolean)
+        console.log(data)
     }, [data]);
 
     return (
         <main className="flex flex-col h-screen overflow-hidden">
             <CommandMenu/>
             <div className="flex flex-row flex-grow overflow-y-auto">
-                <PageList uuid={"userUuid"}/>
+                <PageList uuid={data?.id}/>
 
                 <div className="flex-grow flex flex-col overflow-y-auto">
                     <Navbar uuid={"userUuid"}/>
-                    <Editor uuid={uuid} owner={"userUuid"}></Editor>
+                    <Editor uuid={uuid} owner={data?.id}></Editor>
                 </div>
             </div>
         </main>
