6aadc66532e0af338ca13c4754c719a5e2a500aa
YuInseo <inseo0121@gmail.com>
2024-04-20 18:06:15 +0900

    feat: renamed variables name & handle cell click

--- app/components/props/table.tsx
+++ app/components/props/table.tsx
@@ -1,25 +1,53 @@
 "use client"
-
+import {useEffect} from "react";
+import {ContextMenuDemo} from "@/app/components/props/context-demo";
+import {cn} from "@/app/lib/utils";
 
 interface TableProp {
-    tableData: string[][];
+    rows: string[][];
 }
 
-export const Table = ({tableData}: TableProp) => {
+export const Table = ({rows}: TableProp) => {
+
+    useEffect(() => {
+        console.log(rows)
+    }, []);
+
+
+    const handleClick = () => {
+
+    }
+
+
     return (
         <table>
-            <tbody>
+            <tbody className="border-separate">
             {
-                tableData.map((cells, key) => {
+                rows.map((cells, rowPosition) => {
+
+                    console.log(`rows `, rowPosition)
                     return (
-                        <tr key={key}>
+                        <tr key={rowPosition} onClick={() => {
+                            console.log("clicked row", rowPosition)
+                        }}>
                             {
-                                cells.map((cell, key) => {
+                                cells.map((cell, cellPosition) => {
+                                    console.log(`key`, cellPosition)
+
                                     return (
-                                        <td key={key}>
-                                            {
-                                                cell
-                                            }
+                                        <td key={cellPosition}
+                                            className="border border-separate border-slate-700"
+                                            suppressContentEditableWarning={true}
+                                            contentEditable={true} onClick={() => {
+                                            handleClick();
+                                            console.log("clicked cell", cellPosition)
+                                        }}>
+                                            <div className={""}>
+                                                {
+                                                    cell
+                                                }
+                                            </div>
+
                                         </td>
                                     )
                                 })
