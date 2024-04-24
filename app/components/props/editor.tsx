7b43b8ab4bb088e03a1c048a9fed3a0bebf0e9a8
YuInseo <inseo0121@gmail.com>
2024-04-24 14:06:09 +0900

    feat: edited editor

--- app/components/props/editor.tsx
+++ app/components/props/editor.tsx
@@ -1,12 +1,10 @@
 "use client"
 
-import {InputTitle} from "@/app/components/ui/input-title";
-import React, {useEffect, useRef, useState} from "react";
-import {useQuery} from "react-query";
-import {Button} from "@/app/components/ui/button";
-import {Block} from "@/app/components/props/block";
-import {LoadPages} from "@/app/lib/page/page";
-import {CreateBlock, LoadAllBlock} from "@/app/lib/block/block";
+import "@/app/components/props/editor/style.css"
+import React, {useState} from "react";
+import Image from "next/image";
+import {InputTest} from "@/app/components/props/input-test";
+import {Profile} from "@/app/components/profile";
 
 interface EditorProp {
     uuid: string;
@@ -14,65 +12,37 @@ interface EditorProp {
 }
 
 export const Editor: React.FC<EditorProp> = ({uuid, owner}: EditorProp) => {
-    const {data, error} = useQuery(['pages', uuid], async () => await LoadPages(uuid));
-    const [currentPage, setCurrentPage] = useState<Page>();
-    const [blocks, setBlocks] = useState<IBlock[]>([]);
-    const [selectedIndex, setSelectedIndex] = useState<number>();
-    const [selectCount, setSelectCount] = useState<number>(0);
+    // const {data, error} = useQuery(['pages', uuid], async () => await LoadPages(uuid));
+    const [position, setPosition] = useState<Position>();
 
-    useEffect(() => {
-        const filteredPages = data?.filter(page => page.metadata.url === uuid);
+    const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        let viewportOffset = event.currentTarget.getBoundingClientRect();
 
-        if (filteredPages !== undefined) {
-            setCurrentPage(filteredPages[0])
-        }
-    }, [data]);
+        let top = viewportOffset.top;
+        let left = viewportOffset.left;
 
-    useEffect(() => {
-        LoadAllBlock(uuid).then(blocks => {
-            setBlocks(blocks)
-        });
-    }, []);
-
-    const addNewBlock = () => {
-        CreateBlock(uuid).then();
-        let new_block: IBlock = {
-            content: [],
-            isEditing: false,
-            pageId: ""
-        }
-
-        setBlocks(prevBlocks => [...prevBlocks, new_block]);
-    };
-
-    const onDeleteHandler = () => {
-
+        setPosition({
+            x: left,
+            y: top
+        })
     }
 
     return (
-        <div className="flex-grow overflow-y-auto">
-            <div className="max-w-screen-xl mx-auto px-8">
-                <div className="w-full max-w-[1200px] sm:max-w-[700px] mx-auto">
-                    <div className="flex">
-                        <Button variant="ghost">아이콘 추가</Button>
-                        <Button variant="ghost">커버 추가</Button>
-                        <Button variant="ghost">댓글 추가</Button>
-                    </div>
-
-                    <InputTitle url={uuid} title={currentPage?.metadata.title} onEnter={addNewBlock}
-                                onDelete={onDeleteHandler}/>
-
+        <div className="">
+            <div className="grid grid-cols-[250px_auto] grid-rows-[50px_auto] gap-1 w-full overflow-hidden">
+                <span className="col-start-1">
                     {
-                        blocks?.map((block, index) => {
-                            return (
-                                <Block block={block} key={index}
-                                       url={uuid}
-                                       index={index}
-                                       setSelectedIndex={setSelectedIndex}/>
-                            )
-                        })
+                        position &&
+                        <Profile position={position}/>
                     }
-
+                </span>
+                <span className="">
+                <h1 className="font-bold text-4xl">
+                    Untitled
+                </h1>
+                </span>
+                <div className="col-start-2 w-fit">
+                    <InputTest handleClick={handleClick}></InputTest>
                 </div>
             </div>
         </div>
