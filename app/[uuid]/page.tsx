86e555c156d0a6de3af87d0d4e260cd9796f1643
YuInseo <inseo0121@gmail.com>
2024-04-24 19:08:04 +0900

    feat: added Providers

--- app/[uuid]/page.tsx
+++ app/[uuid]/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import {Client} from "@/app/components/props/client";
+import ProfileProvider from "@/app/context/ProfileProvider";
+import BlockProvider from "@/app/context/BlockProvider";
 
 export default function Home({params: {uuid}}: { params: { uuid: string } }) {
 
@@ -9,6 +11,10 @@ export default function Home({params: {uuid}}: { params: { uuid: string } }) {
     //TODO check if API server is down
 
     return (
-        <Client uuid={uuid}/>
+        <ProfileProvider>
+            <BlockProvider>
+                <Client uuid={uuid}/>
+            </BlockProvider>
+        </ProfileProvider>
     );
 }
