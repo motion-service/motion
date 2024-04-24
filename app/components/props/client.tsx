26c15a3ce713ae2c496a5c49a50e5dbfae500ef4
YuInseo <inseo0121@gmail.com>
2024-04-24 19:06:51 +0900

    feat: disabled PageList for a while

--- app/components/props/client.tsx
+++ app/components/props/client.tsx
@@ -9,21 +9,15 @@ import {useAccount} from "@/app/hook/useAccount";
 import {cookies} from "next/headers";
 
 export const Client = ({uuid}: ClientProp) => {
-
     let {data} = useAccount();
-
-    useEffect(() => {
-        console.log(data)
-    }, [data]);
 
     return (
         <main className="flex flex-col h-screen overflow-hidden">
             <CommandMenu/>
             <div className="flex flex-row flex-grow overflow-y-auto">
-                <PageList uuid={data?.id}/>
+                {/*<PageList uuid={data?.id}/>*/}
 
                 <div className="flex-grow flex flex-col overflow-y-auto">
-                    <Navbar uuid={"userUuid"}/>
                     <Editor uuid={uuid} owner={data?.id}></Editor>
                 </div>
             </div>
