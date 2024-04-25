f5978bd50a63933caa3994c3eb273cc8af251d84
YuInseo <inseo0121@gmail.com>
2024-04-25 13:51:42 +0900

    feat: edit page list prop

--- app/components/props/page-list.tsx
+++ app/components/props/page-list.tsx
@@ -1,56 +1,47 @@
 "use client"
-import React, {useEffect, useState} from "react";
-import {FaPlus} from "react-icons/fa";
+import React, {useState} from "react";
 import {useQuery} from "react-query";
-import {MdOutlineContactPage} from "react-icons/md";
-import Link from "next/link";
 import {LoadPages} from "@/app/lib/page/page";
-
+import Image from "next/image";
+import {PageListItem} from "@/app/components/ui/page-list-item";
+import {ToggleGroup} from "@/app/components/ui/toggle-group";
+import {FaPlus} from "react-icons/fa";
+import {Button} from "@/app/components/ui/button";
 
 interface PageListProp {
     uuid: string;
 }
 
-
 export const PageList = ({uuid}: PageListProp) => {
-    const [isHovered, setHovered] = useState<boolean>();
-    const {data, error} = useQuery(['pages', uuid], async () => await LoadPages(uuid));
-
-    const handleCreatePage = () => {
-        fetch(`http://localhost:8090/client/page/create/${uuid}`, {
-            method: 'POST'
-        }).then()
-    }
 
     return (
-        <aside className="w-48 bg-[#202020] h-full text-white overflow-y-auto p-2 cursor-pointer">
-            <div className="text-[11px]"
-                 onMouseEnter={() => {
-                     setHovered(true)
-                 }}
-                 onMouseLeave={() => {
-                     setHovered(false)
-                 }}
-                // Add CSS to prevent text selection
-                 style={{userSelect: 'none'}}
-            >
-            <span className="flex items-center justify-between" onClick={handleCreatePage}>
-                하위 페이지 생성 {isHovered && <FaPlus/>}
-            </span>
-                <ul>
-                    {
-                        data?.map((value, index) => {
-                            return (
-                                <li key={index}>
-                                    <Link href={value.metadata.url} className="flex h-fit items-center">
-                                        <MdOutlineContactPage/>{value.metadata.title}
-                                    </Link>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-            </div>
-        </aside>
+        <>
+            <aside
+                className="select-none bg-[#202020] text-blue-100 w-64 absolute left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
+
+                <div className="px-2 py-2 text-white text-[14px] flex items-center space-x-2">
+                    <Image src={"https://avatars.githubusercontent.com/u/11320397"} alt={""} width={24} height={24}
+                           className="rounded-[5px]"/>
+                    <span>
+                         Israel Becker86's Motion
+                    </span>
+                </div>
+                <div className="m-2">
+                    <div className="flex items-center w-full justify-between">
+                        <div className="text-[12px]">
+                            Private
+                        </div>
+
+                        <Button variant="ghost" size={"sm"}>
+                            <FaPlus size={10}/>
+                        </Button>
+                    </div>
+                    <ToggleGroup type="single" defaultValue={uuid}>
+                        <PageListItem title={"Untitlesaffasasfdasasff"} uuid={uuid}></PageListItem>
+                        <PageListItem title={"asdf"} uuid={uuid}></PageListItem>
+                    </ToggleGroup>
+                </div>
+            </aside>
+        </>
     )
 }
