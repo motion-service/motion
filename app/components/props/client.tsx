35300d13f2d6ed5a502a43f3d610e4c67d25965a
YuInseo <inseo0121@gmail.com>
2024-04-26 18:05:19 +0900

    feat: added providers on client.tsx

--- app/components/props/client.tsx
+++ app/components/props/client.tsx
@@ -3,15 +3,25 @@
 import {useAccount} from "@/app/hook/useAccount";
 import {PageList} from "@/app/components/props/page-list";
 import {Editor} from "@/app/components/props/editor";
-import React from "react";
+import React, {useEffect} from "react";
+import {socket, SocketContext} from "@/app/context/SocketProvider";
+import ProfileProvider from "@/app/context/ProfileProvider";
+import BlockProvider from "@/app/context/BlockProvider";
 
-export const Client = ({uuid}: ClientProp) => {
+export const Client = () => {
     let {data} = useAccount();
 
     return (
         <div className="relative h-screen overflow-hidden md:flex">
-            <PageList uuid={data?.id}/>
-            <Editor uuid={uuid} owner={data?.id}></Editor>
+            <SocketContext.Provider value={socket}>
+                <PageList uuid={data?.id}/>
+
+                <ProfileProvider>
+                    <BlockProvider>
+                        <Editor/>
+                    </BlockProvider>
+                </ProfileProvider>
+            </SocketContext.Provider>
         </div>
     )
 }
