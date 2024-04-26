8abfc3a558a2610bd437fd5a78d43995541ce207
YuInseo <inseo0121@gmail.com>
2024-04-26 18:07:01 +0900

    feat: added event handlers

--- app/components/props/input-test.tsx
+++ app/components/props/input-test.tsx
@@ -1,71 +1,105 @@
 "use client"
 
-import React, {useContext, useEffect, useState} from "react";
+import React, {useContext, useState} from "react";
 import {cn} from "@/app/lib/utils";
 import {BlockContext} from "@/app/context/BlockProvider";
 import {ProfileContext} from "@/app/context/ProfileProvider";
+import {clsx} from "clsx";
+
 
 interface TestProp {
     className?: string;
+    block?: ITestBlock;
+    selectedIndex: number;
 }
 
-export const InputTest = ({className}: TestProp) => {
+export const InputTest = ({className, block, selectedIndex}: TestProp) => {
     let blockContext = useContext(BlockContext);
     let profileContext = useContext(ProfileContext);
+    const [timerId, setTimerId] = useState<number | null>(null);
+    const [isClicked, setIsClicked] = useState<boolean>();
+
+    const handleMouseEnter = () => {
+        // const id = window.setTimeout(() => {
+        //     //TODO
+        //     setTimerId(null);
+        // }, 2000);
+        //
+        // setTimerId(id);
+    };
+
+    const handleMouseLeave = () => {
+        if (timerId !== null) {
+            clearTimeout(timerId);
+            setTimerId(null);
+        }
+    };
+
+    const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        let rect = event.currentTarget.getBoundingClientRect();
+        let x = rect.x;
+        let y = rect.y;
+
+        profileContext?.setNewPosition({position: {x: x, y: y}})
+    }
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+        let key = event.key;
+
+        setIsClicked(true);
+
+        if (event.key === "a" && event.ctrlKey) {
+            blockContext?.addSelectedAllCount();
+        }
+
+        switch (key) {
+            case "Tab":
+                event.preventDefault();
+
+                break;
+
+            case "Enter":
+                event.preventDefault();
+
+                break;
+
+            case "Backspace":
+                event.preventDefault();
+
+                break;
+        }
+    }
 
     return (
-        <>
-            <div className={cn("focus:outline-none", className)}
-                 contentEditable={true}
-                 suppressContentEditableWarning={true}
-                 suppressHydrationWarning={true}
 
-                 onClick={event => {
-                     let rect = event.currentTarget.getBoundingClientRect();
-                     let x = rect.x;
-                     let y = rect.y;
+        <div className={
+            cn("focus:outline-none"
+                // clsx({
+                //     ["cursor-text"]: timerId !== null,
+                //     ["cursor-pointer select-none"]: timerId === null,
+                //
+                // }
+                , className)}
 
-                     profileContext?.setNewPosition({position: {x: x, y: y}})
-                 }}
+             onDoubleClick={() => {
+                 console.log("test", timerId)
+                 clearTimeout(timerId!);
+             }}
 
-                 onKeyDown={(event) => {
-                     let key = event.key;
-
-                     if (event.key === "a" && event.ctrlKey) {
-                         blockContext?.addSelectedAllCount();
-                     }
-
-                     switch (key) {
-                         case "Tab":
-                             event.preventDefault();
-
-                             break;
-
-                         case "Enter":
-                             event.preventDefault();
-
-                             break;
-
-                         case "Backspace":
-                             event.preventDefault();
-
-                             break;
-                     }
-                 }}
-
-                 onMouseEnter={(event) => {
-
-                 }}
-
-                 onMouseLeave={(event) => {
-
-                 }}
-            >
-
-            </div>
+             contentEditable
+             suppressContentEditableWarning={true}
+             suppressHydrationWarning={true}
+             onClick={handleClick}
+             onKeyDown={handleKeyDown}
+             onMouseEnter={handleMouseEnter}
+             onMouseLeave={handleMouseLeave}
+             onInput={(event) => {
+                 let value = event.currentTarget.innerText;
+                 blockContext?.sendData(selectedIndex, value);
+             }}>
             {
-
+                block?.value
             }
-        </>
+        </div>
     )
 }
