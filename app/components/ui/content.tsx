8a7cd6cad3463496cf4dd8f0f40b2af1f34fa644
YuInseo <inseo0121@gmail.com>
2024-04-26 18:05:55 +0900

    feat: change fake array to real data

--- app/components/ui/content.tsx
+++ app/components/ui/content.tsx
@@ -1,15 +1,21 @@
 import {InputTest} from "@/app/components/props/input-test";
-import React from "react";
+import React, {useContext, useEffect} from "react";
+import {BlockContext} from "@/app/context/BlockProvider";
 
 export const Content = () => {
-    
+    let blockContext = useContext(BlockContext);
+
     return (
         <div className="row-start-2 col-start-2 break-words">
+            {/*<InputTest key={1}></InputTest>*/}
             {
-                Array.from({length: 5}).map((value, index) => (
-
-                    <InputTest key={index}></InputTest>
-                ))
+                blockContext?.blocks ? (
+                    blockContext.blocks.map((value, index) => (
+                        <InputTest key={index} block={value} selectedIndex={index}></InputTest>
+                    ))
+                ) : (
+                    <p>No blocks available</p>
+                )
             }
         </div>
     )
