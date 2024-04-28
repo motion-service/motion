4acafc9eb37f6d622f281793f67f7182aa7d37a1
YuInseo <inseo0121@gmail.com>
2024-04-28 11:51:31 +0900

    feat: removed unnecessary providers

--- app/[uuid]/page.tsx
+++ app/[uuid]/page.tsx
@@ -1,20 +1,13 @@
 "use client"
 
 import {Client} from "@/app/components/props/client";
-import ProfileProvider from "@/app/context/ProfileProvider";
-import BlockProvider from "@/app/context/BlockProvider";
+import UniqueProvider from "@/app/context/UniqueProvider";
 
 export default function Home({params: {uuid}}: { params: { uuid: string } }) {
 
-    //TODO https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions-and-mutations server comp 이해 하기
-    //TODO check url is UUID type
-    //TODO check if API server is down
-
     return (
-        <ProfileProvider>
-            <BlockProvider>
-                <Client uuid={uuid}/>
-            </BlockProvider>
-        </ProfileProvider>
+        <UniqueProvider uuid={uuid}>
+            <Client/>
+        </UniqueProvider>
     );
 }
